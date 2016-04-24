8328ebc59b917bc6157ab64f73f87260a48385aa
Damien Goblot <damiengoblot@gmail.com>
2016-04-23 20:51:21 -0400

    Initial commit: * Hello class with hello string message * sqrt with block and private sub functions * tailrec factorial and gcd implementation

---
+++ src/main/scala/demo/Hello.scala
@@ -0,0 +1,39 @@
+package demo
+
+import scala.annotation.tailrec
+
+/**
+  * Created by damiengoblot on 2016-04-23.
+  */
+class Hello {
+  def sayHello(name: String) = s"Hello, $name!"
+
+  def abs(x: Double) = if (x > 0) x else -x
+
+  def sqrt(x: Double) = {
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
+
+    def isGoodEnough(guess: Double) =
+      abs(guess * guess - x) / x < 0.001
+
+    def improve(guess: Double) =
+      (guess + x / guess) / 2
+
+    sqrtIter(1.0)
+  }
+
+  def factorial(n: Int): Int = {
+    @tailrec
+    def loop(acc: Int, n: Int): Int =
+      if (n == 0) acc
+      else loop(acc * n, n - 1)
+    loop(1, n)
+  }
+
+  @tailrec
+  final def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+}
---
+++ src/main/scala/hello.sc
@@ -0,0 +1,16 @@
+import demo.Hello
+
+val hello = new Hello
+
+print(hello.sayHello("Damien"))
+
+(1).+(2)
+
+hello.sqrt(2)
+hello.sqrt(4)
+hello.sqrt(1e-6)
+hello.sqrt(1e60)
+
+hello.factorial(6)
+
+hello.gcd(12, 70)
